74977135f2e87a0b3750947592db4d052acc73d6
Farid Dibirov <farid962@gmail.com>
2025-05-22 21:57:11 +0400

    grading calculator part 2, fibo series tests

---
+++ SparkyNUnitTest/FiboNUnitTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+namespace Sparky;
+
+
+
+
+[TestFixture]
+public class FiboNUnitTests
+{
+    private Fibo fibo;
+
+    [SetUp]
+    public void Setup()
+    {
+        fibo = new Fibo();
+    }
+
+
+    [Test]
+    public void FiboChecker_Range1_ReturnsFiboSeries ()
+    {
+        fibo.Range = 1;
+
+        var result = fibo.GetFiboSeries();
+
+        List<int> expectedFibo = new() { 0 };
+
+        Assert.That(result, Is.Not.Empty);
+        Assert.That(result, Is.Ordered);
+        Assert.That(result, Is.EquivalentTo(expectedFibo));
+
+    }
+
+
+
+    [Test]
+    public void FiboChecker_Range6_ReturnsFiboSeries()
+    {
+        fibo.Range = 6;
+
+        var result = fibo.GetFiboSeries();
+
+        List<int> expectedFibo = new() {0,1,1,2,3,5};
+
+        Assert.That(result, Does.Contain(3));
+        Assert.That(result.Count, Is.EqualTo(6) );
+        Assert.That(result, Has.No.Member(4));
+        Assert.That(result, Is.EquivalentTo(expectedFibo));
+
+    }
+
+}
--- SparkyNUnitTest/GradingCalculatorNUnitTests.cs
+++ SparkyNUnitTest/GradingCalculatorNUnitTests.cs
@@ -16,6 +16,71 @@ public class GradingCalculatorNUnitTests
         gradingCalculator = new GradingCalculator();
     }
 
- 
+
+    [Test]
+    public void GetGrade_InputScore95Attendance90_ReturnsGrade()
+    {
+        gradingCalculator.Score = 95;
+        gradingCalculator.AttendancePercentage = 90;
+
+        Assert.That(gradingCalculator.GetGrade(), Is.EqualTo("A"));
+    }
+
+
+    [Test]
+    public void GetGrade_InputScore85Attendance90_ReturnsGrade()
+    {
+        gradingCalculator.Score = 85;
+        gradingCalculator.AttendancePercentage = 90;
+
+        Assert.That(gradingCalculator.GetGrade(), Is.EqualTo("B"));
+    }
+
+    [Test]
+    public void GetGrade_InputScore65Attendance90_ReturnsGrade()
+    {
+        gradingCalculator.Score = 65;
+        gradingCalculator.AttendancePercentage = 90;
+
+        Assert.That(gradingCalculator.GetGrade(), Is.EqualTo("C"));
+    }
+
+    [Test]
+    public void GetGrade_InputScore95Attendance65_ReturnsGrade()
+    {
+        gradingCalculator.Score = 95;
+        gradingCalculator.AttendancePercentage = 65;
+
+        Assert.That(gradingCalculator.GetGrade(), Is.EqualTo("B"));
+    }
+
+    [Test]
+    [TestCase(95,55)]
+    [TestCase(65,55)]
+    [TestCase(50,90)]
+    public void GetGrade_FailureScenarios_ReturnsGrade(int score, int attendance)
+    {
+        gradingCalculator.Score = score;
+        gradingCalculator.AttendancePercentage = attendance;
+
+        Assert.That(gradingCalculator.GetGrade(), Is.EqualTo("F"));
+    }
+
+
+    [Test]
+    [TestCase(95, 90, ExpectedResult ="A")]
+    [TestCase(85, 90, ExpectedResult = "B")]
+    [TestCase(65, 90, ExpectedResult = "C")]
+    [TestCase(95, 65, ExpectedResult = "B")]
+    [TestCase(95, 55, ExpectedResult = "F")]
+    [TestCase(65, 55, ExpectedResult = "F")]
+    [TestCase(50, 90, ExpectedResult = "F")]
+    public string GetGrade_InputScoreAttendance_ReturnsGrade(int score, int attendance)
+    {
+        gradingCalculator.Score = score;
+        gradingCalculator.AttendancePercentage = attendance;
+
+       return gradingCalculator.GetGrade();
+    }
 
 }
