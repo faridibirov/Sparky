18c3cf1d9564f9d19500deef917d89bbde26d12b
Farid Dibirov <farid962@gmail.com>
2025-05-22 22:16:29 +0400

    setup bankaccount class, setup logger class, integration test demo, unit test with fake logger

---
+++ Sparky/BankAccount.cs
@@ -0,0 +1,41 @@
+﻿namespace Sparky;
+
+public class BankAccount
+{
+
+    public int balance {  get; set; }  
+
+    public readonly ILogBook _logBook;
+
+    public BankAccount(ILogBook logBook)
+    {
+        _logBook = logBook;
+        balance = 0;
+    }
+
+    public bool Deposit(int amount)
+    {
+        _logBook.Message("Depoist invoked");
+        balance += amount;
+        return true;
+    }
+
+
+    public bool Withdraw(int amount)
+    {
+        if (amount <= balance)
+        {
+            balance -= amount;
+            return true;
+        }
+
+        return false;
+    }
+
+
+    public int GetBalance()
+    {
+        return balance;
+    }
+
+}
---
+++ SparkyNUnitTest/BankAccountNUnitTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace Sparky;
+
+[TestFixture]
+public class BankAccountNUnitTests
+{
+    private BankAccount bankAccount;
+
+
+    [SetUp]
+    public void Setup()
+    {
+        bankAccount = new(new LogFaker());
+    }
+
+
+
+    [Test]
+    public void BankDeposit_Add100_ReturnTrue()
+    {
+        var result = bankAccount.Deposit(100);
+
+        ClassicAssert.IsTrue(result);
+
+        Assert.That(bankAccount.balance, Is.EqualTo(100));
+    }
+
+}
