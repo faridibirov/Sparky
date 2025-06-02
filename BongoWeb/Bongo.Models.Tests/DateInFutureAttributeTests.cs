0beea451d77354fb1fd1ce2397f258d7cebfeff6
Farid Dibirov <farid962@gmail.com>
2025-06-02 23:57:35 +0400

    bongo models date checker

---
+++ BongoWeb/Bongo.Models.Tests/DateInFutureAttributeTests.cs
@@ -0,0 +1,20 @@
+﻿using Bongo.Models.ModelValidations;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace Bongo.Models;
+
+
+[TestFixture]
+public class DateInFutureAttributeTests
+{
+    [Test]
+    public void DateValidator_InputExpectedDateRange_DateValidity()
+    {
+        DateInFutureAttribute dateInFutureAttribute = new(()=>DateTime.Now);
+
+        var result = dateInFutureAttribute.IsValid(DateTime.Now.AddSeconds(-100));
+
+        ClassicAssert.AreEqual(true, result);
+    }
+}
