9da0b49947e8497372daeea863c5a4cb4a6f7dc5
Farid Dibirov <farid962@gmail.com>
2025-06-03 22:43:09 +0400

    bongo models data checker part2, data access unit tests, setup bongo core unit test

---
+++ BongoWeb/Bongo.Core.Tests/StudyRoomBookingServiceTests.cs
@@ -0,0 +1,26 @@
+﻿
+
+using Bongo.Core.Services;
+using Bongo.DataAccess.Repository.IRepository;
+using Moq;
+using NUnit.Framework;
+
+namespace Bongo.Core;
+
+[TestFixture]
+public class StudyRoomBookingServiceTests
+{
+    private Mock<IStudyRoomBookingRepository> _studyRoomBookingRepositoryMock;
+    private Mock<IStudyRoomRepository> _studyRoomRepositoryMock;
+    private StudyRoomBookingService _bookingService;
+
+    [SetUp]
+    public void Setup()
+    {
+        _studyRoomBookingRepositoryMock = new Mock<IStudyRoomBookingRepository>();
+        _studyRoomRepositoryMock = new Mock<IStudyRoomRepository>();
+        _bookingService = new StudyRoomBookingService(
+            _studyRoomBookingRepositoryMock.Object,
+            _studyRoomRepositoryMock.Object);
+    }
+}
